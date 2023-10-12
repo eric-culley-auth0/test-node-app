cfb6377bbec7ece867b5a66f3deafea350d0b78d
Eric Culley <eric.culley@auth0.com>
2023-10-12 00:45:16 -0600

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import 'dotenv/config';
+import { AuthenticationClient } from 'auth0';
+
+const rl = readline.createInterface({ input, output });
+
+const auth0Domain = process.env.AUTH0_DOMAIN;
+const auth0ClientId = process.env.AUTH0_CLIENT_ID;
+const auth0ClientSecret = process.env.AUTH0_CLIENT_SECRET;
+
+const auth0 = new AuthenticationClient({
+    domain: auth0Domain,
+    clientId: auth0ClientId,
+    clientSecret: auth0ClientSecret,
+});
+
+const main = async () => {
+    const answer1 = await rl.question('Enter email address to recieve code: ');
+
+    const res1 = await auth0.passwordless.sendEmail({
+        email: answer1,
+        send: 'code'
+    })
+    console.log("Send Email Response: ", res1.status);
+
+    const answer2 = await rl.question('Enter code to recieve tokens: ');
+
+    const res2 = await auth0.passwordless.loginWithEmail({
+        email: answer1,
+        code: answer2
+    })
+    console.log("Login Response: ", res2.data);
+
+    rl.close();
+}
+
+main();
+
+
+
